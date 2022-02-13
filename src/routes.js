82b62ae52f20825b99e0a453c77f406edac17d4b
rami96rihani <rami.rihani@esprit.tn>
2022-02-13 21:21:22 +0100

    Revert "Revert "card""
    This reverts commit bb186d873c4f71f3c97be7150d6dbaefed1a8911.

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import Products from './pages/Products';
 import Blog from './pages/Blog';
 import User from './pages/User';
 import NotFound from './pages/Page404';
+import AddNewCard from './pages/AddNewCard'
 
 // ----------------------------------------------------------------------
 
@@ -23,7 +24,8 @@ export default function Router() {
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
         { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> }
+        { path: 'blog', element: <Blog /> },
+        { path: 'card', element: <AddNewCard /> }
       ]
     },
     {
