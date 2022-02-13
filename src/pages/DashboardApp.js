3649b536f66ce5683dbb9b38e0acb26456fb503e
rami96rihani <rami.rihani@esprit.tn>
2022-02-13 21:47:54 +0100

    azz

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,50 +1,21 @@
 // material
-
-import { Box, Grid, Container, Typography } from '@mui/material';
-
+import {Link as RouterLink} from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import plusFill from '@iconify/icons-eva/plus-fill';
 import {
-  ProductImgStyle,
-  Link,
-  Box,
-  Grid,
   Card,
   CardContent,
   CardActions,
   CardMedia,
-  Table,
   Stack,
-  Avatar,
   Button,
-  Checkbox,
-  TableRow,
-  TableBody,
-  TableCell,
   Container,
   Typography,
-  TableContainer,
-  TablePagination
+
 } from '@mui/material';
 
 // components
 import Page from '../components/Page';
-import {
-  AppTasks,
-  AppNewUsers,
-  AppBugReports,
-  AppItemOrders,
-  AppNewsUpdate,
-  AppWeeklySales,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppCurrentSubject,
-  AppConversionRates
-} from '../components/_dashboard/app';
-import { color } from '@mui/system';
-import { red } from '@mui/material/colors';
 
 // ----------------------------------------------------------------------
 
